cc8788079fe5eaee314fd7b95680e5a14261e31a
Yoana Chung <yoana@yoanachung.com>
2023-02-08 13:28:13 -0800

    사용자 입력 폼 만들기

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,20 +1,44 @@
 <template>
-  <form action="">
+  <form v-on:submit.prevent="submitForm">
     <div>
       <label for="username">id: </label>
-      <input id="username" type="text">
+      <input id="username" v-model="username" type="text">
     </div>
     <div>
       <label for="password">pw: </label>
-      <input id="password" type="password">
+      <input id="password" v-model="password" type="password">
     </div>
-    <button>login</button>
+    <button type="submit">login</button>
   </form>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm: function() {
+      console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password
+      };
+      axios.post(url, data)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  }
 }
 </script>
 
