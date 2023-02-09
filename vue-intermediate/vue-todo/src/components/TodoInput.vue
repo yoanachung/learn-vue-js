0b9cbf0a7ef0662ff356844926aca34c0cab2cbb
Yoana Chung <yoana@yoanachung.com>
2023-02-08 17:19:30 -0800

    TodoInput 컴포넌트 - 등록

--- vue-intermediate/vue-todo/src/components/TodoInput.vue
+++ vue-intermediate/vue-todo/src/components/TodoInput.vue
@@ -1,15 +1,54 @@
 <template>
-  <div>
-    input
+  <div class="inputBox shadow">
+    <input v-model="newTodoItem" v-on:keyup.enter="addTodo" type="text">
+    <span v-on:click="addTodo" class="addContainer">
+      <i class="fas fa-plus addBtn"></i>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
-
+  data: function() {
+    return {
+      newTodoItem: ""
+    }
+  },
+  methods: {
+    addTodo: function() {
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput: function() {
+      this.newTodoItem = "";
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #64adfb, #7096ff);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
 </style>
