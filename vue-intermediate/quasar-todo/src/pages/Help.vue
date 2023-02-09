7e034a6d21a6c3cd36ca2f3b49dbb51f3e492cd0
Yoana Chung <yoana@yoanachung.com>
2023-02-09 13:30:15 -0800

    페이지 내용 수정

--- vue-intermediate/quasar-todo/src/pages/Help.vue
+++ vue-intermediate/quasar-todo/src/pages/Help.vue
@@ -1,6 +1,9 @@
 <template>
-  <q-page>
-    <h5>Help</h5>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Spread love everywhere you go. Let no one ever come to you without leaving happier. -Mother Teresa</p>
+    <p>When you reach the end of your rope, tie a knot in it and hang on. -Franklin D. Roosevelt</p>
+    <p>Always remember that you are absolutely unique. Just like everyone else. -Margaret Mead</p>
   </q-page>
 </template>
 
