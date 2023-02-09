6f712056c8ac58815d76399337556a61c08bab82
Yoana Chung <yoana@yoanachung.com>
2023-02-09 12:59:16 -0800

    헤더 이미지 추가

--- vue-intermediate/quasar-todo/src/layouts/MainLayout.vue
+++ vue-intermediate/quasar-todo/src/layouts/MainLayout.vue
@@ -17,6 +17,9 @@
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
+      <q-img
+        src="../statics/mountains.jpeg"
+        class="header-image absolute-top"/>
     </q-header>
 
     <q-drawer
@@ -107,3 +110,12 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="scss">
+.header-image {
+  height: 100%;
+  z-index: -1;
+  opacity: 0.2;
+  filter: grayscale(100%);
+}
+</style>
