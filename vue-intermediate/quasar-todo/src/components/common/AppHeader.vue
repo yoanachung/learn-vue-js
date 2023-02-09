b8a68cb79f3eda08bd430c5d6baf9c4b6e372919
Yoana Chung <yoana@yoanachung.com>
2023-02-09 15:48:52 -0800

    refactor: 헤더 컴포넌트 분리

---
+++ vue-intermediate/quasar-todo/src/components/common/AppHeader.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-header elevated>
+    <q-toolbar>
+      <q-btn
+        flat
+        dense
+        round
+        icon="menu"
+        aria-label="Menu"
+        @click="toggleDrawer"
+      />
+
+      <q-toolbar-title> Quasar App </q-toolbar-title>
+
+      <div>Quasar v{{ $q.version }}</div>
+    </q-toolbar>
+    <div class="q-px-lg q-pt-xl">
+      <div class="text-h3">TODO</div>
+      <div class="text-subtitle1">{{ todaysDate }}</div>
+    </div>
+    <q-img
+      src="../../statics/mountains.jpeg"
+      class="header-image absolute-top"
+    />
+  </q-header>
+</template>
+
+<script>
+import { date } from "quasar";
+
+export default {
+  computed: {
+    todaysDate() {
+      const timeStamp = Date.now();
+      return date.formatDate(timeStamp, "ddd MMM D, YYYY");
+    },
+  },
+  methods: {
+    toggleDrawer() {
+      this.$emit("toggleDrawer");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.header-image {
+  height: 100%;
+  z-index: -1;
+  opacity: 0.2;
+  filter: grayscale(100%);
+}
+</style>
--- vue-intermediate/quasar-todo/src/layouts/MainLayout.vue
+++ vue-intermediate/quasar-todo/src/layouts/MainLayout.vue
@@ -1,31 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-      <div class="q-px-lg q-pt-xl">
-        <div class="text-h3">TODO</div>
-        <div class="text-subtitle1">{{ todaysDate }}</div>
-      </div>
-      <q-img
-        src="../statics/mountains.jpeg"
-        class="header-image absolute-top"/>
-    </q-header>
-
+    <AppHeader v-on:toggleDrawer="toggleDrawer"></AppHeader>
     <q-drawer
         v-model="leftDrawerOpen"
         show-if-above
@@ -76,31 +51,26 @@
 </template>
 
 <script>
-import { date } from 'quasar'
+import AppHeader from '../components/common/AppHeader.vue'
 
 export default {
   name: 'MainLayout',
   components: {
+    AppHeader
   },
   data () {
     return {
       leftDrawerOpen: false
     }
   },
-  computed: {
-    todaysDate() {
-      const timeStamp = Date.now()
-      return date.formatDate(timeStamp, 'ddd MMM D, YYYY')
+  methods: {
+    toggleDrawer() {
+      this.leftDrawerOpen = !this.leftDrawerOpen;
     }
   }
 }
 </script>
 
-<style scoped lang="scss">
-.header-image {
-  height: 100%;
-  z-index: -1;
-  opacity: 0.2;
-  filter: grayscale(100%);
-}
+<style>
+
 </style>
