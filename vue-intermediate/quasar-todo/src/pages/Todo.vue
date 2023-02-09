85569151d7512e7fd2416ab0e32d6d54d1a2d404
Yoana Chung <yoana@yoanachung.com>
2023-02-09 14:15:53 -0800

    목록 표시

--- vue-intermediate/quasar-todo/src/pages/Todo.vue
+++ vue-intermediate/quasar-todo/src/pages/Todo.vue
@@ -1,10 +1,77 @@
 <template>
-  <q-page class="q-pa-lg">
-    <h5 class="q-mt-none">Todo</h5>
+  <q-page class="bg-grey-3 column">
+    <q-list 
+      class="bg-white"
+      separator
+      border>
+      <q-item 
+        v-for="task in tasks" 
+        :key="task.title"
+        @click="task.done = !task.done"
+        :class="{ 'done bg-blue-1' : task.done }"
+        clickable
+        v-ripple>
+        <q-item-section avatar>
+          <q-checkbox 
+            v-model="task.done" 
+            class="no-pointer-events"
+            color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+          <q-item-label caption>{{ task.description }}</q-item-label>
+        </q-item-section>
+        <q-item-section
+          v-if="task.done"
+          side>
+          <q-btn 
+            flat 
+            round 
+            dense 
+            color="primary" 
+            icon="delete" />
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      tasks: [
+        {
+          title: "qwer",
+          description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,`,
+          done: false
+        },
+        {
+          title: "asdf",
+          description: "",
+          done: true
+        },
+        {
+          title: "zxcv",
+          description: `Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+            non proident, Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+            non proident`,
+          done: false
+        }
+      ]
+    }
+  }
 }
 </script>
+
+<style scoped lang="scss">
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: #bbb;
+  }
+}
+</style>
