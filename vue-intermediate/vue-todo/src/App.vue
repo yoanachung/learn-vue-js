68061c3fb6bf3f6fe7ae9c8bf59f802ad76dcb81
Yoana Chung <yoana@yoanachung.com>
2023-02-08 22:39:12 -0800

    refactor: 목록 전부 삭제 기능 - 데이터 관리 로직을 App.vue로 이동

--- vue-intermediate/vue-todo/src/App.vue
+++ vue-intermediate/vue-todo/src/App.vue
@@ -5,7 +5,7 @@
     <TodoList v-bind:propsdata="todoItems" 
         v-on:removeItem="removeOneItem"
         v-on:toggleItem="toggleOneItem"></TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoFooter v-on:clearAll="clearAllItems"></TodoFooter>
   </div>
 </template>
 
@@ -39,6 +39,10 @@ export default {
       this.todoItems[index].completed = !this.todoItems[index].completed;
       localStorage.removeItem(todoItem.item);
       localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+    clearAllItems: function() {
+      this.todoItems = [];
+      localStorage.clear();
     }
   },
   created: function() {
