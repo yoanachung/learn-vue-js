9a8ce323b0dda189a7c91dcaa1b41121f6c22dfc
Yoana Chung <yoana@yoanachung.com>
2023-02-09 16:20:45 -0800

    refactor: 리스트 컴포넌트 분리

---
+++ vue-intermediate/quasar-todo/src/components/todo/TodoList.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-list class="bg-white" separator border>
+    <q-item
+      v-for="(task, index) in propsdata"
+      :key="task.title"
+      @click="task.done = !task.done"
+      :class="{ 'done bg-blue-1': task.done }"
+      clickable
+      v-ripple
+    >
+      <q-item-section avatar>
+        <q-checkbox
+          v-model="task.done"
+          class="no-pointer-events"
+          color="primary"
+        />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>{{ task.title }}</q-item-label>
+      </q-item-section>
+      <q-item-section v-if="task.done" side>
+        <q-btn
+          @click.stop="deleteTask(index)"
+          flat
+          round
+          dense
+          color="primary"
+          icon="delete"
+        />
+      </q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script>
+export default {
+  props: ["propsdata"],
+  methods: {
+    deleteTask() {
+      this.$emit("deleteTask")
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- vue-intermediate/quasar-todo/src/pages/Todo.vue
+++ vue-intermediate/quasar-todo/src/pages/Todo.vue
@@ -29,39 +29,10 @@
       </q-dialog>
     </div>
 
-    <q-list 
-      class="bg-white"
-      separator
-      border>
-      <q-item 
-        v-for="(task, index) in tasks" 
-        :key="task.title"
-        @click="task.done = !task.done"
-        :class="{ 'done bg-blue-1' : task.done }"
-        clickable
-        v-ripple>
-        <q-item-section avatar>
-          <q-checkbox 
-            v-model="task.done" 
-            class="no-pointer-events"
-            color="primary" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>{{ task.title }}</q-item-label>
-        </q-item-section>
-        <q-item-section
-          v-if="task.done"
-          side>
-          <q-btn 
-            @click.stop="deleteTask(index)"
-            flat 
-            round 
-            dense 
-            color="primary" 
-            icon="delete" />
-        </q-item-section>
-      </q-item>
-    </q-list>
+    <TodoList 
+      v-bind:propsdata="tasks"
+      v-on:deleteTask="deleteTask"></TodoList>
+
     <div
       v-if="!tasks.length" 
       class="no-tasks absolute-center">
@@ -73,6 +44,8 @@
 </template>
 
 <script>
+import TodoList from '../components/todo/TodoList.vue'
+
 export default {
   data() {
     return {
@@ -104,6 +77,9 @@ export default {
       })
       this.newTask = ""
     }
+  },
+  components: {
+    TodoList
   }
 }
 </script>
