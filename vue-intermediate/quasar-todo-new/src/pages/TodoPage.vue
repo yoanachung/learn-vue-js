76c57fc6d0e322b32ac79f3d56da127cec5bd3e5
Yoana Chung <yoana@yoanachung.com>
2023-02-10 13:10:13 -0800

    todo 페이지 추가

---
+++ vue-intermediate/quasar-todo-new/src/pages/TodoPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="row items-center justify-evenly"> TODO </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+});
+</script>
