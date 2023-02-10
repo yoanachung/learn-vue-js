7aa36bfea7aa65fac5feb2e15f978af301fc7414
Yoana Chung <yoana@yoanachung.com>
2023-02-09 16:30:54 -0800

    refactor: 추가 컴포넌트 분리

---
+++ vue-intermediate/quasar-todo/src/components/todo/TodoAdd.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="row q-pa-sm bg-white">
+    <q-btn
+      @click="prompt = true"
+      class="addBtn"
+      round
+      color="primary"
+      icon="add"
+    />
+    <q-dialog v-model="prompt" persistent>
+      <q-card style="min-width: 500px">
+        <q-card-section>
+          <div class="text-h6">New task</div>
+        </q-card-section>
+        <q-card-section class="q-pt-none">
+          <q-input
+            dense
+            v-model="newTask"
+            autofocus
+            placeholder="What's your new task?"
+            @keyup.enter="addTask"
+          />
+        </q-card-section>
+        <q-card-actions align="right" class="text-primary">
+          <q-btn flat label="Cancel" v-close-popup />
+          <q-btn flat label="Add task" @click="addTask" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTask: "",
+      prompt: false
+    }
+  },
+  methods: {
+    addTask() {
+      this.$emit("addTask", this.newTask)
+      this.newTask = ""
+      this.prompt = false
+    }
+  }
+};
+</script>
+
+<style scoped>
+.addBtn {
+  margin: 6px 5px auto auto;
+}
+</style>
