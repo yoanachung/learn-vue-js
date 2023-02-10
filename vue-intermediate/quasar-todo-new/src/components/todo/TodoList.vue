9b9c68c6e146a588b4d7be1b6625440509f42c7a
Yoana Chung <yoana@yoanachung.com>
2023-02-10 15:22:50 -0800

    목록

---
+++ vue-intermediate/quasar-todo-new/src/components/todo/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="task in todoTasks" :key="task.id">
+        {{ task.id }} - {{ task.title }} - {{ task.done }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { TodoTask } from 'components/models';
+import { defineComponent, ref } from 'vue';
+import { useTodoStore } from '../../stores/todo-store';
+
+export default defineComponent({
+  setup() {
+    const todoStore = useTodoStore();
+    const todoTasks = ref<TodoTask[]>(todoStore.tasks);
+    return { todoTasks };
+  },
+});
+</script>
+
+<style></style>
--- vue-intermediate/quasar-todo-new/src/pages/TodoPage.vue
+++ vue-intermediate/quasar-todo-new/src/pages/TodoPage.vue
@@ -1,14 +1,16 @@
 <template>
   <q-page class="row items-center justify-evenly">
     <TodoAdd></TodoAdd>
+    <TodoList></TodoList>
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import TodoList from '../components/todo/TodoList.vue';
 import TodoAdd from '../components/todo/TodoAdd.vue';
 
 export default defineComponent({
-  components: { TodoAdd },
+  components: { TodoList, TodoAdd },
 });
 </script>
