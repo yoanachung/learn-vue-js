8adb4180e6dae68bb30448f0c8b1bd5b9ccab067
Yoana Chung <yoana@yoanachung.com>
2023-02-09 17:15:23 -0800

    refactor: 네비게이션바 컴포넌트 분리

--- vue-intermediate/quasar-todo/src/layouts/MainLayout.vue
+++ vue-intermediate/quasar-todo/src/layouts/MainLayout.vue
@@ -1,46 +1,7 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <AppHeader v-on:toggleDrawer="toggleDrawer"></AppHeader>
-    <q-drawer
-        v-model="leftDrawerOpen"
-        show-if-above
-        :width="250"
-        :breakpoint="700"
-      >
-        <q-scroll-area style="height: calc(100% - 176px); margin-top: 176px; border-right: 1px solid #ddd">
-          <q-list padding>
-            <q-item to="/" exact clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="list" />
-              </q-item-section>
-
-              <q-item-section>
-                Todo
-              </q-item-section>
-            </q-item>
-
-            <q-item to="/help" exact clickable v-ripple>
-              <q-item-section avatar>
-                <q-icon name="help" />
-              </q-item-section>
-
-              <q-item-section>
-                Help
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-scroll-area>
-
-        <q-img class="absolute-top" src="../statics/mountains.jpeg" style="height: 176px">
-          <div class="absolute-bottom bg-transparent">
-            <q-avatar size="56px" class="q-mb-sm">
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-            </q-avatar>
-            <div class="text-weight-bold">Yoana Chung</div>
-            <div>@yoanachung</div>
-          </div>
-        </q-img>
-      </q-drawer>
+    <AppNav v-bind:propsdata="leftDrawerOpen"></AppNav>
 
     <q-page-container>
       <keep-alive>
@@ -52,15 +13,17 @@
 
 <script>
 import AppHeader from '../components/common/AppHeader.vue'
+import AppNav from '../components/common/AppNav.vue'
 
 export default {
   name: 'MainLayout',
   components: {
-    AppHeader
+    AppHeader,
+    AppNav
   },
   data () {
     return {
-      leftDrawerOpen: false
+      leftDrawerOpen: true
     }
   },
   methods: {
