efe519e9c82ea2f0e19a5e9456c6f46ade5cc541
Yoana Chung <yoana@yoanachung.com>
2023-02-13 09:54:18 -0800

    rename TodoAdd to TodoAddButton

--- vue-intermediate/quasar-todo-new/src/pages/TodoPage.vue
+++ vue-intermediate/quasar-todo-new/src/pages/TodoPage.vue
@@ -1,11 +1,11 @@
 <template>
   <q-page class="bg-grey-3 column">
-    <TodoAdd></TodoAdd>
+    <TodoAddButton></TodoAddButton>
     <TodoList></TodoList>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import TodoList from '../components/todo/TodoList.vue';
-import TodoAdd from '../components/todo/TodoAdd.vue';
+import TodoAddButton from '../components/todo/TodoAddButton.vue';
 </script>
