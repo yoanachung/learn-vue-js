78199c68a06c91026047c66dac58d8a79d49a21d
Yoana Chung <yoana@yoanachung.com>
2023-02-13 15:29:19 -0800

    replace @keyup with <q-form> and <q-btn type="submit">

--- vue-intermediate/quasar-todo-new/src/components/todo/TodoAddButton.vue
+++ vue-intermediate/quasar-todo-new/src/components/todo/TodoAddButton.vue
@@ -3,22 +3,27 @@
     <q-btn @click="prompt = true" color="primary" icon="add" round />
     <q-dialog v-model="prompt" @hide="closeDialog">
       <q-card class="full-width">
-        <q-card-section>
-          <div class="text-h6">New task</div>
-        </q-card-section>
-        <q-card-section class="q-pt-none">
-          <q-input
-            dense
-            v-model="newTitle"
-            autofocus
-            placeholder="What's your new task?"
-            @keypress.enter="addTask"
-          />
-        </q-card-section>
-        <q-card-actions align="right" class="text-primary">
-          <q-btn flat label="Cancel" @click="closeDialog" />
-          <q-btn flat label="Add task" @click="addTask" />
-        </q-card-actions>
+        <q-form @submit="addTask">
+          <q-card-section>
+            <div class="text-h6">New task</div>
+          </q-card-section>
+          <q-card-section class="q-pt-none">
+            <q-input
+              filled
+              autofocus
+              v-model="newTitle"
+              label="What's your new task?"
+              lazy-rules
+              :rules="[
+                (val) => (val && val.length > 0) || 'Please type something',
+              ]"
+            />
+          </q-card-section>
+          <q-card-actions align="right" class="text-primary">
+            <q-btn flat label="Cancel" @click="closeDialog" />
+            <q-btn flat label="Add task" type="submit" color="primary" />
+          </q-card-actions>
+        </q-form>
       </q-card>
     </q-dialog>
   </div>
