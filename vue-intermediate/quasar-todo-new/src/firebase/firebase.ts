1c24990bd7ff7ae6a9bc9421cf832a80d5bc14e2
Yoana Chung <yoana@yoanachung.com>
2023-02-15 16:19:24 -0800

    init firebase

---
+++ vue-intermediate/quasar-todo-new/src/firebase/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBbjyRWTrC1w4X7dAGNBMqSkz_YUh8mK0w',
+  authDomain: 'todo-36098.firebaseapp.com',
+  projectId: 'todo-36098',
+  storageBucket: 'todo-36098.appspot.com',
+  messagingSenderId: '331789891603',
+  appId: '1:331789891603:web:9f002d4f2421b3fa32e7e6',
+  measurementId: 'G-WTKEJGHXT3',
+  databaseURL: 'https://todo-36098-default-rtdb.firebaseio.com/',
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const firebaseDB = getDatabase(firebaseApp);
+
+export default firebaseDB;
